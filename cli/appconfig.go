fredlawl/200-colony-movie-night-bot

commit f56f632a357e47070cb7df403cbcce8a6b18bbbd

Separate user defined configuration from settings generation
Separate user defined configuration from settings generation.

cli/appconfig.go
@@ -4,64 +4,87 @@ import (
 	"time"
 )
 
-type AppState int32
+type PeriodName int32
 
 const (
-	SUGGESTING AppState = iota
+	SUGGESTING PeriodName = iota
 	VOTING
 	MOVIENIGHT
 	SLEEP
 )
 
+// Configuration is based on a single 7 day week, with the seggestion
+// date starting on Monday.
 type AppConfig struct {
-	suggestionDayStart     time.Weekday
+	localization           string
 	suggestionPeriodInDays int
 	votePeriodInDays       int
 	movieNightPeriodInDays int
-	appState               AppState
-	timeLocation           time.Location
-	isoYear                int
-	isoWeek                int
-	daysLeftInCurrentState int
-	now                    time.Time
 }
 
-func CreateAppConfig() *AppConfig {
-	loc, _ := time.LoadLocation("America/Chicago")
-	cfg := AppConfig{
-		suggestionDayStart:     time.Monday,
+type Period struct {
+	name     PeriodName
+	daysLeft int
+}
+
+type AppSettings struct {
+	curPeriod    Period
+	localization time.Location
+	isoYear      int
+	isoWeek      int
+	curDay       time.Time // This is the current day with no time.
+}
+
+func DefaultConfiguration() AppConfig {
+	return AppConfig{
+		localization:           "America/Chicago",
 		suggestionPeriodInDays: 3,
 		votePeriodInDays:       1,
 		movieNightPeriodInDays: 1,
-		appState:               SLEEP,
-		timeLocation:           *loc,
 	}
-
-	now := time.Now().In(loc)
-	cfg.now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, loc)
-	cfg.appState, cfg.daysLeftInCurrentState = calculateState(cfg, cfg.now)
-	cfg.isoYear, cfg.isoWeek = now.ISOWeek()
-
-	return &cfg
 }
 
-func calculateState(cfg AppConfig, now time.Time) (curState AppState, daysLeft int) {
+func CreateAppSettings(cfg AppConfig) AppSettings {
+	loc, _ := time.LoadLocation(cfg.localization)
+
+	settings := AppSettings{
+		localization: *loc,
+	}
+
+	now := time.Now().In(&settings.localization)
+	settings.curDay = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0,
+		0, &settings.localization)
+	settings.isoYear, settings.isoWeek = now.ISOWeek()
+	settings.curPeriod = calculatePeriod(cfg, settings.curDay)
+
+	return settings
+}
+
+func calculatePeriod(cfg AppConfig, now time.Time) Period {
+	curPeriod := Period{
+		name:     SLEEP,
+		daysLeft: 0,
+	}
+
 	suggestionPeriodStart := now.AddDate(0, 0, -int(now.Weekday()))
 	suggestionPeriodEnd := suggestionPeriodStart.AddDate(0, 0, cfg.suggestionPeriodInDays)
 	votingPeriodEnd := suggestionPeriodEnd.AddDate(0, 0, cfg.votePeriodInDays)
 	movieNightPeriodEnd := votingPeriodEnd.AddDate(0, 0, cfg.movieNightPeriodInDays)
 
 	if now.After(suggestionPeriodStart) && now.Before(suggestionPeriodEnd.AddDate(0, 0, 1)) {
-		return SUGGESTING, int(suggestionPeriodEnd.Sub(now).Hours()) / 24
+		curPeriod.name = SUGGESTING
+		curPeriod.daysLeft = int(suggestionPeriodEnd.Sub(now).Hours()) / 24
 	}
 
 	if now.After(suggestionPeriodEnd) && now.Before(votingPeriodEnd.AddDate(0, 0, 1)) {
-		return VOTING, int(votingPeriodEnd.Sub(now).Hours()) / 24
+		curPeriod.name = VOTING
+		curPeriod.daysLeft = int(votingPeriodEnd.Sub(now).Hours()) / 24
 	}
 
 	if now.After(votingPeriodEnd) && now.Before(movieNightPeriodEnd.AddDate(0, 0, 1)) {
-		return MOVIENIGHT, int(movieNightPeriodEnd.Sub(now).Hours()) / 24
+		curPeriod.name = MOVIENIGHT
+		curPeriod.daysLeft = int(movieNightPeriodEnd.Sub(now).Hours()) / 24
 	}
 
-	return SLEEP, 0
+	return curPeriod
 }

cli/main.go
@@ -10,17 +10,15 @@ import (
 // TODO: Use github.com/urfave/cli for cli configuration
 
 func main() {
-	log.Println("before create")
-	cfg := CreateAppConfig()
-	log.Println("after create")
+	cfg := DefaultConfiguration()
+	settings := CreateAppSettings(cfg)
 
 	db, err := bolt.Open("cli.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("test")
-	log.Println(cfg.appState)
+	log.Println(settings.curPeriod.name)
 
 	defer db.Close()
 }

cli/appconfig_test.go
@@ -6,61 +6,66 @@ import (
 )
 
 func TestGivenASuggestionPeriodDateStateIsInSuggesting(t *testing.T) {
-	cfg := CreateAppConfig()
-	now := time.Date(2021, 4, 5, 0, 0, 0, 0, &cfg.timeLocation)
+	cfg := DefaultConfiguration()
+	settings := CreateAppSettings(cfg)
+	now := time.Date(2021, 4, 5, 0, 0, 0, 0, &settings.localization)
 
 	expected := SUGGESTING
-	actual, daysLeft := calculateState(*cfg, now)
+	actual := calculatePeriod(cfg, now)
 
-	if expected != actual || daysLeft != 2 {
+	if expected != actual.name || actual.daysLeft != 2 {
 		t.Fail()
 	}
 }
 
 func TestGivenAEndSuggestionPeriodDateStateIsInSuggesting(t *testing.T) {
-	cfg := CreateAppConfig()
-	now := time.Date(2021, 4, 7, 0, 0, 0, 0, &cfg.timeLocation)
+	cfg := DefaultConfiguration()
+	settings := CreateAppSettings(cfg)
+	now := time.Date(2021, 4, 7, 0, 0, 0, 0, &settings.localization)
 
 	expected := SUGGESTING
-	actual, daysLeft := calculateState(*cfg, now)
+	actual := calculatePeriod(cfg, now)
 
-	if expected != actual || daysLeft != 0 {
+	if expected != actual.name || actual.daysLeft != 0 {
 		t.Fail()
 	}
 }
 
 func TestGivenAVotingDateStateIsInVoting(t *testing.T) {
-	cfg := CreateAppConfig()
-	now := time.Date(2021, 4, 8, 0, 0, 0, 0, &cfg.timeLocation)
+	cfg := DefaultConfiguration()
+	settings := CreateAppSettings(cfg)
+	now := time.Date(2021, 4, 8, 0, 0, 0, 0, &settings.localization)
 
 	expected := VOTING
-	actual, daysLeft := calculateState(*cfg, now)
+	actual := calculatePeriod(cfg, now)
 
-	if expected != actual || daysLeft != 0 {
+	if expected != actual.name || actual.daysLeft != 0 {
 		t.Fail()
 	}
 }
 
 func TestGivenAMovieNightDateStateIsInMovienight(t *testing.T) {
-	cfg := CreateAppConfig()
-	now := time.Date(2021, 4, 9, 0, 0, 0, 0, &cfg.timeLocation)
+	cfg := DefaultConfiguration()
+	settings := CreateAppSettings(cfg)
+	now := time.Date(2021, 4, 9, 0, 0, 0, 0, &settings.localization)
 
 	expected := MOVIENIGHT
-	actual, daysLeft := calculateState(*cfg, now)
+	actual := calculatePeriod(cfg, now)
 
-	if expected != actual || daysLeft != 0 {
+	if expected != actual.name || actual.daysLeft != 0 {
 		t.Fail()
 	}
 }
 
 func TestGivenASleepDateStateIsInSleep(t *testing.T) {
-	cfg := CreateAppConfig()
-	now := time.Date(2021, 4, 10, 0, 0, 0, 0, &cfg.timeLocation)
+	cfg := DefaultConfiguration()
+	settings := CreateAppSettings(cfg)
+	now := time.Date(2021, 4, 10, 0, 0, 0, 0, &settings.localization)
 
 	expected := SLEEP
-	actual, daysLeft := calculateState(*cfg, now)
+	actual := calculatePeriod(cfg, now)
 
-	if expected != actual || daysLeft != 0 {
+	if expected != actual.name || actual.daysLeft != 0 {
 		t.Fail()
 	}
 }

commit af0090e8dc26fb9a085a0e6a8b4f4c85eb034a0c

Handle localization error
Commit f56f632 ("Separate user defined configuration from settings
generation") introduced the ability for dynamic configuration to be
injected into CreateAppSettings(). A bug may be introduced if the string
localization that's passed is invalid, therefore, handle the error
situation.

cli/appconfig.go
@@ -44,8 +44,12 @@ func DefaultConfiguration() AppConfig {
 	}
 }
 
-func CreateAppSettings(cfg AppConfig) AppSettings {
-	loc, _ := time.LoadLocation(cfg.localization)
+func CreateAppSettings(cfg AppConfig) (*AppSettings, error) {
+	loc, locErr := time.LoadLocation(cfg.localization)
+
+	if locErr != nil {
+		return nil, locErr
+	}
 
 	settings := AppSettings{
 		localization: *loc,
@@ -57,7 +61,7 @@ func CreateAppSettings(cfg AppConfig) AppSettings {
 	settings.isoYear, settings.isoWeek = now.ISOWeek()
 	settings.curPeriod = calculatePeriod(cfg, settings.curDay)
 
-	return settings
+	return &settings, nil
 }
 
 func calculatePeriod(cfg AppConfig, now time.Time) Period {

cli/main.go
@@ -13,12 +13,17 @@ import (
 
 func main() {
 	cfg := DefaultConfiguration()
-	settings := CreateAppSettings(cfg)
+	settings, settingsErr := CreateAppSettings(cfg)
+
+	if settingsErr != nil {
+		log.Fatal(settingsErr)
+	}
+
 	_ = settings
 
-	db, err := bolt.Open("cli.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
+	db, dbErr := bolt.Open("cli.db", 0600, nil)
+	if dbErr != nil {
+		log.Fatal(dbErr)
 	}
 
 	app := &cli.App{
@@ -45,7 +50,7 @@ func main() {
 
 	cliErr := app.Run(os.Args)
 	if cliErr != nil {
-		log.Fatal(err)
+		log.Fatal(cliErr)
 	}
 
 	defer db.Close()

cli/appconfig_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestGivenASuggestionPeriodDateStateIsInSuggesting(t *testing.T) {
 	cfg := DefaultConfiguration()
-	settings := CreateAppSettings(cfg)
+	settings, _ := CreateAppSettings(cfg)
 	now := time.Date(2021, 4, 5, 0, 0, 0, 0, &settings.localization)
 
 	expected := SUGGESTING
@@ -20,7 +20,7 @@ func TestGivenASuggestionPeriodDateStateIsInSuggesting(t *testing.T) {
 
 func TestGivenAEndSuggestionPeriodDateStateIsInSuggesting(t *testing.T) {
 	cfg := DefaultConfiguration()
-	settings := CreateAppSettings(cfg)
+	settings, _ := CreateAppSettings(cfg)
 	now := time.Date(2021, 4, 7, 0, 0, 0, 0, &settings.localization)
 
 	expected := SUGGESTING
@@ -33,7 +33,7 @@ func TestGivenAEndSuggestionPeriodDateStateIsInSuggesting(t *testing.T) {
 
 func TestGivenAVotingDateStateIsInVoting(t *testing.T) {
 	cfg := DefaultConfiguration()
-	settings := CreateAppSettings(cfg)
+	settings, _ := CreateAppSettings(cfg)
 	now := time.Date(2021, 4, 8, 0, 0, 0, 0, &settings.localization)
 
 	expected := VOTING
@@ -46,7 +46,7 @@ func TestGivenAVotingDateStateIsInVoting(t *testing.T) {
 
 func TestGivenAMovieNightDateStateIsInMovienight(t *testing.T) {
 	cfg := DefaultConfiguration()
-	settings := CreateAppSettings(cfg)
+	settings, _ := CreateAppSettings(cfg)
 	now := time.Date(2021, 4, 9, 0, 0, 0, 0, &settings.localization)
 
 	expected := MOVIENIGHT
@@ -59,7 +59,7 @@ func TestGivenAMovieNightDateStateIsInMovienight(t *testing.T) {
 
 func TestGivenASleepDateStateIsInSleep(t *testing.T) {
 	cfg := DefaultConfiguration()
-	settings := CreateAppSettings(cfg)
+	settings, _ := CreateAppSettings(cfg)
 	now := time.Date(2021, 4, 10, 0, 0, 0, 0, &settings.localization)
 
 	expected := SLEEP