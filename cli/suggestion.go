2a161dd940e8f1fa206567b9d6af4d635db9ddf8
Frederick Lawler <fred@fredlawl.com>
2021-04-10 23:20:35 -0500

    WIP: Begin implementation of create suggestion

--- cli/suggestion.go
+++ cli/suggestion.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"github.com/boltdb/bolt"
 	"github.com/urfave/cli/v2"
 )
 
 type Suggestion struct {
 	userName string
 	movie    string
+}
+
+func suggestMovieAction(c *cli.Context) error {
+	cfg := DefaultConfiguration()
+	settings, settingsErr := CreateAppSettings(cfg)
+
+	if settingsErr != nil {
+		return settingsErr
+	}
+
+	if settings.curPeriod.name != SUGGESTING {
+		_, writeErr := c.App.Writer.Write([]byte("Sorry, unable to add the movie to suggestions. The suggestion period has already ended.\n"))
+		return writeErr
+	}
+
+	db, dbErr := bolt.Open("cli.db", 0600, nil)
+	if dbErr != nil {
+		return dbErr
+	}
+
+	defer db.Close()
+	return nil
 }
 
 func SuggestionCliCommand() *cli.Command {
@@ -32,6 +55,7 @@ Add suggestion:
 				Name:    "suggest",
 				Aliases: []string{"s"},
 				Usage:   "Suggest a movie",
+				Action:  suggestMovieAction,
 			},
 		},
 	}
